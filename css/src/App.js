079b8894d464f75daa6e4cf4f65ad79ce3a34b8f
MJ-Dev92 <minjae920802@naver.com>
2023-01-09 09:29:14 +0900

    리액트 기본

---
+++ css/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Button1 from "./components/Button1";
+import Button2 from "./components/Button2";
+import TailwindComponent from "./TaliwindComponent";
+
+function App() {
+  return (
+    <>
+      <Button1 />
+      <Button2 />
+      <TailwindComponent />
+    </>
+  );
+}
+
+export default App;
