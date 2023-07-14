ea64b63d619dda722e57fe94181212ba83b77e72
MJ-Dev92 <minjae920802@naver.com>
2023-07-14 23:37:33 +0900

    에러 상태 추가

--- basic/src/basic/components/Products.jsx
+++ basic/src/basic/components/Products.jsx
@@ -1,22 +1,31 @@
 import React, { useEffect, useState } from "react";
 
 export default function Products() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
   const [products, setProducts] = useState([]);
   const [checked, setChecked] = useState(false);
   const handleChange = () => setChecked((prev) => !prev);
 
   useEffect(() => {
+    setLoading(true);
+    setError(undefined);
     fetch(`data/${checked ? "sale_" : ""}products.json`)
       .then((res) => res.json())
       .then((data) => {
         console.log("데이터 받아옴");
         setProducts(data);
-      });
+      })
+      .catch((e) => setError("에러가 발생했음"))
+      .finally(() => setLoading(false));
     return () => {
       console.log("청소했음");
     };
   }, [checked]);
 
+  if (loading) return <p>Loading...</p>;
+
+  if (error) return <p>{error}</p>;
   return (
     <>
       <input type="checkbox" value={checked} onChange={handleChange} />
