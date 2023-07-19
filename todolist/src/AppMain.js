ac8645a74963c7f0b762be0b5d2d15345617a315
MJ-Dev92 <minjae920802@naver.com>
2023-07-20 03:00:41 +0900

    헤더 기능

--- todolist/src/AppMain.js
+++ todolist/src/AppMain.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import TodoList from "./components/TodoList/TodoList";
+import Header from "./components/Header/Header";
+
+const filters = ["all", "active", "completed"];
 
 export default function AppMain() {
+  const [filter, setFilter] = useState(filters[0]);
   return (
     <>
-      <TodoList />
+      <Header filters={filters} filter={filter} onFilterChange={setFilter} />
+      <TodoList filter={filter} />
     </>
   );
 }
