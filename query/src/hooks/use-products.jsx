d322cd0889af1f313fcbafb65bf3a077bc16c500
MJ-Dev92 <minjae920802@naver.com>
2023-09-20 15:24:09 +0900

    react_query

---
+++ query/src/hooks/use-products.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+export default function useProducts({ salesOnly }) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    console.log("fatching...");
+    setLoading(true);
+    setError(undefined);
+    fetch(`data/${salesOnly ? "sale_" : ""}products.json`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("데이터 받아옴");
+        setProducts(data);
+      })
+      .catch((e) => setError("에러가 발생했음"))
+      .finally(() => setLoading(false));
+    return () => {
+      console.log("청소했음");
+    };
+  }, [salesOnly]);
+
+  return [loading, error, products];
+}
